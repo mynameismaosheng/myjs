0ef7dcd8b7d744c865b66a7036e9c8c4901ff3be
Maunsell <982431279@qq.com>
2018-05-14 15:59:31 +0800

    write node complate cros and router api

---
+++ node/server/api.js
@@ -0,0 +1,18 @@
+var express = require("express");  
+var http = require("http");  
+var app = express();  
+var router = express.Router();  
+var testRouter =  require('./router');  
+  
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization, Accept,X-Requested-With');
+
+    next();
+}
+app.use(allowCrossDomain)
+
+app.use('/api', testRouter);  
+http.createServer(app).listen(3000);  
+console.log('localhost:3000/api/getData')
